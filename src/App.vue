aa5e9ce1518ad684fe7d85f8c81f10245e16e718
GregLeBarbar <gregory.charmier.epfl.ch>
2017-06-04 17:06:33 +0200

    video 22 : Appréhender les évènements de Vue.js

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  height: 1000px;
+  display: block;
 }
 </style>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,17 +1,38 @@
 <template>
   <!-- On est obligé d'avoir un div englobant -->
   <div class="game">
-    Espace de jeux
+    <span class="round" v-on:click="clickOnRound"></span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'game'
+  name: 'game',
+  methods: {
+    clickOnRound: function () {
+      console.log('click')
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.game {
+  width: 100%;
+  height: 50%;
+  display: block;
+  background: darkslategrey;
+}
+
+.round {
+  width: 50px;
+  height: 50px;
+  background: aliceblue;
+  border-radius: 9999px;
+  position: absolute;
+  margin: 20% 20%;  
+}
 
 </style>
