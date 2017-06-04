28920fd1c5d61758dc1c7d41f0aa03f49ca3830c
GregLeBarbar <gregory.charmier.epfl.ch>
2017-06-04 18:04:29 +0200

    video 26 :  Écouter le clavier

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,31 @@
 <template>
   <!-- On est obligé d'avoir un div englobant -->
   <div class="game">
-    <span class="round" v-on:click="clickOnRound"></span>
+    <!-- j'ai remplacé la touche alt par la touche ctrl car alt ne fonctionnait pas -->
+    <span class="round" v-on:click="clickOnRound" v-on:click.ctrl="bonus"></span>
   </div>
 </template>
 
 <script>
 export default {
   name: 'game',
+  created: function () {
+    // this.start est une méthode 
+    document.onkeydown = this.start
+  },
   methods: {
-    clickOnRound: function () {
-      console.log('click')
+    clickOnRound: function (event) {
+      console.log('CLICK')
+      console.log(event)
+    },
+    bonus: function (event) {
+      console.log('BONUS')
+      console.log(event)
+    },
+    start: function (event) {
+      if (event.key === 'Enter') {
+        console.log('BONUS')
+      }
     }
   }
 }
