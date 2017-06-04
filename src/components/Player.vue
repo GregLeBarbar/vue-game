7ffa1d94125bedc794cf241a7eb04bfc4ffe0496
GregLeBarbar <gregory.charmier.epfl.ch>
2017-06-04 16:42:44 +0200

    video 19 : directive custom

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- On est obligé d'avoir un div englobant -->
-  <div v-bind:class="playerClass">
-    <span>{{ welcomeMessage }}</span>
-    <form>
+  <div>
+    <span v-hide>{{ welcomeMessage }}</span>
+    <form v-hide>
       <input type="text" placeholder="Entrer votre nom" v-border:yellow />
       <button type="submit">Jouer</button>
     </form>
@@ -13,12 +13,23 @@
 export default {
   name: 'player',
   created: function () {
+    this.player = 'Grégory'
     this.playerClass = this.player ? 'player' : 'playerForm'
     this.welcomeMessage = this.player ? 'Bonjour ' + this.player + ' !' : 'Pas de joueur.'
   },
   directives: {
     border: function (el, binding) {
       el.style.borderColor = binding.arg
+    },
+    // creation de la directive custom 'hide'
+    hide: function (el, binding, vnode) {
+      let player = vnode.context.player
+      let isForm = vnode.tag === 'form'
+      if (isForm) {
+        el.style.display = player ? 'none' : 'block'
+      } else {
+        el.style.display = player ? 'block' : 'none'
+      }
     }
   }
 }
@@ -26,10 +37,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.player form {
-  display: none;
-}
-.playerForm span {
-  display: none;
-}
 </style>
