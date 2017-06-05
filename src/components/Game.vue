4617c50a138e6b83bc3bbea3f3fa3f8f783137cc
GregLeBarbar <gregory.charmier.epfl.ch>
2017-06-05 10:51:25 +0200

    Vidéo n°38: Créer un index dans une boucle

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -7,7 +7,7 @@
     </div>
     <div class="log">
       <p v-for="item in collection">
-        {{ item }}
+        #{{ item.id }} - {{ item.msg }}
       </p>
     </div>
   </div>
@@ -42,20 +42,20 @@ export default {
   methods: {
     clickOnRound: function (event) {
       this.click++
-      this.collection.unshift(`BRAVO ! (${this.click})`)
+      this.addLog(`BRAVO ! (${this.click})`)
     },
     bonus: function (event) {
       if (this.bonusActivated) {
         this.click++
-        this.collection.unshift(`PERFECT ! (${this.click}) +2`)
+        this.addLog(`PERFECT ! (${this.click}) +2`)
       } else {
         this.click--
-        this.collection.unshift(`???? (${this.click}) -1`)
+        this.addLog(`???? (${this.click}) -1`)
       }
     },
     clickOnInterface: function (event) {
       this.click--
-      this.collection.unshift(`HO NON :( (${this.click}) -1`)
+      this.addLog(`HO NON :( (${this.click}) -1`)
     },
     start: function (event) {
       if (event.key === 'Enter') {
@@ -72,6 +72,9 @@ export default {
 
       this.roundStyle.height = this.roundStyle.width = `${size}px`
       this.roundStyle.margin = `${top}% ${left}%`
+    },
+    addLog: function (msg) {
+      this.collection.unshift({id: this.collection.length, msg: msg})
     }
   }
 }
